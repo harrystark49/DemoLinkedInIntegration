7ce038c733d4626f39aac86590d7867d396e8d24
Harrystark49 <harikrishna.aravakantha@xcubelabs.com>
2022-02-28 12:16:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/demolinkedin/LinkedInConstants.kt
@@ -0,0 +1,12 @@
+package com.example.demolinkedin
+
+object LinkedInConstants {
+
+    val CLIENT_ID = "86jgtrstdm6btg"
+    val CLIENT_SECRET = "zmhyADjVaUZH36Lx"
+    val REDIRECT_URI = "https://www.linkedin.com/company/79139514/"
+    val SCOPE = "r_liteprofile%20r_emailaddress"
+
+    val AUTHURL = "https://www.linkedin.com/oauth/v2/authorization"
+    val TOKENURL = "https://www.linkedin.com/oauth/v2/accessToken"
+}
